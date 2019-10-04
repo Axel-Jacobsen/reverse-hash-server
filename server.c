1a1643fef8ffce81226f84b11c6a04365a30e9a4
s174293 <kasper.b.joergensen@gmail.com>
2019-10-04 12:38:49 +0000

    removed print-statements from server

--- server.c
+++ server.c
@@ -32,11 +32,11 @@ void read_client_msg(uint8_t *big_endian_arr, uint8_t *response_arr)
 	int num = 1;	
 	if(*(char *)&num == 1)
 	{
-		printf("Little-Endian\n");
+		//printf("Little-Endian\n");
 	}
 	else
 	{
-		printf("Big-Endian\n");
+		//printf("Big-Endian\n");
 	}
 	/*
 	hash = big_endian_arr[0:32];
@@ -52,40 +52,40 @@ void read_client_msg(uint8_t *big_endian_arr, uint8_t *response_arr)
 	sprintf(foo, "%" PRIu64, f);
 	sha256(&f, outbuf);
 
-	printf("0x");
+	//printf("0x");
 	int j;
 	for (j = 0; j < 32; j++)
 	{
-		printf("%02x", outbuf[j]);
+		//printf("%02x", outbuf[j]);
 	}
-	printf("\n");
+	//printf("\n");
 	
 
 	//Process input
 	uint8_t sha256_test[SHA_LEN] = {0};
 	int i;
-	printf("0x");
+	//printf("0x");
 	for (i = 0; i < 32; i++)
 	{
-		printf("%02x", big_endian_arr[i]);
+		//printf("%02x", big_endian_arr[i]);
 	}
-	printf(" ");
+	//printf(" ");
 
 	uint64_t start = 0;
 	for (i = 32; i < 40; i++)
 	{
 		start = start | (big_endian_arr[i] << (8 * (39 - i)));
 	}
-	printf("%lu ", start);
+	//printf("%lu ", start);
 			
 	uint64_t end = 0;
 	for (i = 40; i < 48; i++)
 	{
 		end = end | (big_endian_arr[i] << (8 * (47 - i)));
 	}
-	printf("%lu ", end);
+	//printf("%lu ", end);
 			
-	printf("%d\n", big_endian_arr[48]);
+	//printf("%d\n", big_endian_arr[48]);
 
 	int sha_good = 1;
 	uint64_t k;
@@ -147,7 +147,7 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	printf("\nserving on %s:%d\n", inet_ntoa(server_addr.sin_addr), PORT);
+	//printf("\nserving on %s:%d\n", inet_ntoa(server_addr.sin_addr), PORT);
 
 	struct sockaddr_in client_addr;
 	uint client_address_len = 0;
@@ -166,10 +166,10 @@ int main(int argc, char *argv[])
 		uint8_t *pbuffer = buffer;
 		uint8_t response[RESPONSE_LEN] = {0};
 
-		char *client_ip = inet_ntoa(client_addr.sin_addr);
+		//char *client_ip = inet_ntoa(client_addr.sin_addr);
 		//uint8_t lil_endian[MESSAGE_LEN] = {0};
 
-		printf("\nclient connected with ip address: %s\n", client_ip);
+		//printf("\nclient connected with ip address: %s\n", client_ip);
 		while ((n = recv(sock, pbuffer, maxlen, 0)) > 0)
 		{
 			pbuffer += n;
