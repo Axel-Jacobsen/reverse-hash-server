266389f2524c3da6b6eea77432c6217c2a2af218
Axel Jacobsen <axelnj44@gmail.com>
2019-11-09 19:25:43 +0100

    finish test_server
    Creates res.txt, result file - should be helpful for analysis, debugging

--- test_server.py
+++ test_server.py
@@ -9,25 +9,59 @@
 import subprocess
 
 def process_std_out(s):
-	latencies = []
-	for line in s:
-		if line.startswith('['):
-			latency = line.split(']')[-1].strip()
-			latencies.append(latency)
-		if line.startswith('Results'):
-			score = line.split(' ')[-1]
-	return score, latencies
+	pls = []
+	import time
+
+	line = ''
+
+	for c in s:
+		if c == '\n':
+			if line.startswith('['):
+				bracket_split = line.split(']')
+				priority = int(bracket_split[0].split(' ')[-1])
+				latency  = int(bracket_split[-1].strip())
+				pls.append((priority, latency))
+
+			if line.startswith('Results'):
+				score = int(line.split(' ')[-1].strip())
+
+			line = ''
+
+		else:
+			line += c
+
+	return (score, pls)
 
 if __name__ == '__main__':
 	N = 2
 	avg_score = 0
-	ls = []
-	command = "run-client-milestone.sh"
+
+	scores = []
+	priority_latency_ls = []
+
 	for _ in range(N):
 		print 'starting run %d' % _
-		s = subprocess.check_output([command])
-		score, latencies = process_std_out(s)
-		avg_score += N
-		print '  score  %d' % score
+		commands = ["./run-client.sh"]
+		s = subprocess.check_output(commands)
+		score, pls = process_std_out(s)
+
+		scores.append(score)
+		priority_latency_ls.append(pls)
+
+		avg_score += score
+		print '\tscore  %d\n' % score
+
+	avg_score /= N
+
+	with open('res.txt', 'w') as f:
+		f.write('average score: %d\n' % avg_score)
+		for i in xrange(len(scores)):
+			score = scores[i]
+			pls = priority_latency_ls[i][0]
+
+			f.write('run %d score %d\n' % (i, score))
+			for pl in pls:
+				f.write('\tpriority (p): %d\tlatency (l): %d\tp*l: %d\n' % (pls[0], pls[1], pls[0] * pls[1]))
+			f.write('\n')
 
 	print 'avg score: %d' % avg_score
