61734b5d9242bc83e13bbf0129b922c2a0b211ff
Rasmus <s174246@student.dtu.dk>
2019-11-14 20:55:41 +0000

    updated, now the struct is initialized right away in the main

--- server.c
+++ server.c
@@ -21,11 +21,9 @@
 struct hashBuffer {
 	uint64_t start;
 	uint64_t end;
-	uint8_t hash[MESSAGE_LEN];
-	uint8_t priority;
-	
-	
-}__attribute__((packed));
+	uint8_t hash[SHA_LEN];
+	uint8_t priority;	
+}hash;
 
 
 void sha256(uint64_t *v, unsigned char out_buff[SHA256_DIGEST_LENGTH])
@@ -36,54 +34,47 @@ void sha256(uint64_t *v, unsigned char out_buff[SHA256_DIGEST_LENGTH])
 	SHA256_Final(out_buff, &sha256);
 }
 
-// *big_endian_arr is an array of bytes, response_arr is a pointer to an array of the same size
-void rev_hash(uint8_t *big_endian_arr, uint8_t *response_arr)
+void initStruct(uint8_t *buffer)
 {
-	struct hashBuffer hash, test_hash;
-	
+
 	uint8_t i;
 	uint64_t start = 0;
 	for (i = 32; i < 40; i++)
 	{
-		start = start | (((uint64_t)big_endian_arr[i]) << (8 * (39 - i)));
-	}
-
+		start = start | (((uint64_t)buffer[i]) << (8 * (39 - i)));
+	}		
 	uint64_t end = 0;
 	for (i = 40; i < 48; i++)
 	{
-		end = end | (((uint64_t)big_endian_arr[i]) << (8 * (47 - i)));
+		end = end | (((uint64_t)buffer[i]) << (8 * (47 - i)));
 	}
 	
-	long long hashing = 0;
-	for (i = 0; i < 32; i++)
-	{
-		hashing = hashing | (((uint64_t)big_endian_arr[i]) << (8 * (31 - i)));
-	}
 	//printf("start: %d\n", (int)start);	
 	//printf("hashing: %lld\n", hashing);
 	
 	hash.start = start;
 	hash.end = end;
-	memcpy((void *)hash.hash, big_endian_arr, 32);
-	
-	for (i = 0; i < 32; i++)
-	{
-		//printf("val: %d\n", (int)hash.hash[i]);
-	}	
+	memcpy((void *)hash.hash, buffer, SHA_LEN);
+}
+
+// *big_endian_arr is an array of bytes, response_arr is a pointer to an array of the same size
+void rev_hash(uint8_t *response_arr)
+{
+
 	uint8_t sha_good = 1;
 	uint8_t sha256_test[SHA_LEN] = {0};
 	uint64_t k;
 	uint64_t k_conv;
-
+	
 	for(k = hash.start; k < hash.end; k++){
 		sha_good = 1;
 		sha256(&k, sha256_test);
 		
-		uint64_t test_hashing = 0;
+		uint8_t i;
 		for(i = 0; i < 32; i++)
 		{
-			printf("hash: %d test: %d\n", big_endian_arr[i], sha256_test[i]);
-			if(big_endian_arr[i] != sha256_test[i]){
+			
+			if(hash.hash[i] != sha256_test[i]){
 				sha_good = 0;
 				break;
 			}
@@ -156,11 +147,17 @@ int main(int argc, char *argv[])
 		uint8_t response[RESPONSE_LEN] = {0};
 		while ((n = recv(sock, pbuffer, maxlen, 0)) > 0)
 		{
+
+				
+		
 			pbuffer += n;
 			maxlen -= n;
 			len += n;
+		
 
-			rev_hash(buffer, response);
+
+			initStruct(buffer);
+			rev_hash(response);
 			send(sock, response, RESPONSE_LEN, 0);
 	}
 		close(sock);
