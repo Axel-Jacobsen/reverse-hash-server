bc32773d5a594a489e3f1a6f9168947b47819e4b
Rasmus <s174246@student.dtu.dk>
2019-11-14 18:16:49 +0000

    now works using the struct instead

--- server.c
+++ server.c
@@ -19,10 +19,10 @@
 
 
 struct hashBuffer {
-	int start;
-	int end;
-	long hash;
-	short priority;
+	uint64_t start;
+	uint64_t end;
+	uint8_t hash[MESSAGE_LEN];
+	uint8_t priority;
 	
 	
 }__attribute__((packed));
@@ -54,16 +54,22 @@ void rev_hash(uint8_t *big_endian_arr, uint8_t *response_arr)
 		end = end | (((uint64_t)big_endian_arr[i]) << (8 * (47 - i)));
 	}
 	
-	uint64_t hashing = 0;
+	long long hashing = 0;
 	for (i = 0; i < 32; i++)
 	{
 		hashing = hashing | (((uint64_t)big_endian_arr[i]) << (8 * (31 - i)));
 	}
-
+	//printf("start: %d\n", (int)start);	
+	//printf("hashing: %lld\n", hashing);
+	
 	hash.start = start;
 	hash.end = end;
-	hash.hash = hashing;
-
+	memcpy((void *)hash.hash, big_endian_arr, 32);
+	
+	for (i = 0; i < 32; i++)
+	{
+		//printf("val: %d\n", (int)hash.hash[i]);
+	}	
 	uint8_t sha_good = 1;
 	uint8_t sha256_test[SHA_LEN] = {0};
 	uint64_t k;
@@ -74,13 +80,17 @@ void rev_hash(uint8_t *big_endian_arr, uint8_t *response_arr)
 		sha256(&k, sha256_test);
 		
 		uint64_t test_hashing = 0;
-		for(i = 0; i < SHA_LEN; i++)
+		for(i = 0; i < 32; i++)
 		{
-			test_hashing = test_hashing | (((uint64_t)sha256_test[i]) << (8 * ((SHA_LEN-1) - i)));
+			printf("hash: %d test: %d\n", big_endian_arr[i], sha256_test[i]);
+			if(big_endian_arr[i] != sha256_test[i]){
+				sha_good = 0;
+				break;
+			}
 		}
 		
-		test_hash.hash = test_hashing;
-		if(hash.hash = test_hash.hash){
+		
+		if(sha_good){
 			k_conv = htobe64(k);
 			memcpy(response_arr, &k_conv, sizeof(k_conv));
 			break;
